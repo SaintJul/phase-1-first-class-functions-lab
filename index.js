9d716dd8327ebbaca9c4c0f0a6fdbf92ca8d024d
SaintJul <favor.julius@gmail.com>
2021-05-20 18:29:36 -0400

    commit tree b5a841ce7f772e945aff26922cf9defe1ee910ce

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = arr => {   
+    const copyarr = arr.slice()
+    return copyarr.splice(0,2)    
+    }
+
+const returnLastTwoDrivers = arr => {
+    const copyarr = arr.slice()
+    return copyarr.splice(2,3)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (num) {
+       // console.log(num)
+      const mul = num => num * 5
+        return mul
+   //return function(num){return (num * 5)}   
+}
+
+const fareDoubler = fare => fare * 2
+
+const fareTripler = fare => fare * 3
+
+function selectDifferentDrivers(arry, func){
+   return func(arry)
+}
+
+
